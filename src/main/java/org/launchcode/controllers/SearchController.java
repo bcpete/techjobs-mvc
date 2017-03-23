6cc157438dc7d5e580bf60bcbc48a43a8dc1be8b
hometownzero <bradycpeters@gmail.com>
2017-03-23 16:00:49 -0500

    Added search functionality, and the list all function now works. Can't figure out why my category name section isn't capitalized though. Will come back to that.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.controllers.ListController.columnChoices;
+import static org.launchcode.models.JobData.findByColumnAndValue;
+
 /**
  * Created by LaunchCode
  */
@@ -18,10 +21,28 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    //TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="results")
+    public String results(Model model,
+                          @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", columnChoices);
+        String column = searchType;
+        String value = searchTerm;
+
+        if (column.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(value);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+
+    }
 
 }
